2610fadf221fd1ca9003aa27a8759caa467a6e8c
witek <witekdev@gmail.com>
2012-08-26 14:07:54 +0200

    assessment item custom view first commit.

--- upfront/assessmentitem/content/assessmentitem.py
+++ upfront/assessmentitem/content/assessmentitem.py
@@ -1,7 +1,11 @@
 from five import grok
 from zope import schema
+from Acquisition import aq_inner
+
 from plone.directives import dexterity, form
 from plone.app.textfield import RichText
+
+from Products.CMFCore.utils import getToolByName
 
 from upfront.assessmentitem import MessageFactory as _
 
@@ -18,3 +22,21 @@ class IAssessmentItem(form.Schema):
 class AssessmentItem(dexterity.Container):
     grok.implements(IAssessmentItem)
 
+grok.templatedir('templates')
+class View(dexterity.DisplayForm):
+    grok.context(IAssessmentItem)
+    grok.template('viewassessmentitem')
+    grok.require('zope2.View')
+
+    def assessment_item(self):
+        """ return contents of assessment item (introduction field)
+        """
+        return self.context.introduction.output
+
+    def questions(self):
+        """ return all question objects in the context of assessment item
+        """
+        contentFilter = {
+                        'portal_type':'upfront.assessmentitem.content.question'}
+        questions = self.context.getFolderContents(contentFilter)
+        return questions
