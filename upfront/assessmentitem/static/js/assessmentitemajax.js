33f3fce5de70e8dbb73c3b18b820d1e3d88b19f8
Roché Compaan <roche@upfrontsystems.co.za>
2012-09-22 13:45:12 +0200

    refactor and simplify to use new answerform

--- upfront/assessmentitem/static/js/assessmentitemajax.js
+++ upfront/assessmentitem/static/js/assessmentitemajax.js
@@ -2,47 +2,24 @@ $(document).ready(function() {
 
     var answercount = 0;
 
-    jq("#add_answer").click(function() {
+    $("#add_answer").click(function() {
 
-        // Extract URL from HTML page
-        var answerhtml = jq("#add_answer_ajaxlink a").attr("href");
-        if (answerhtml) {
-            // create the divs where ajax call with reside
-            jq("#answer_template").append('<li><div id="result_answer"></div><div id="result_choice"></div></li>');
-    
-            answercount += 1;
-            answerid = 'form.widgets.answer' + answercount;
-            jq("#result_answer").load(
-                '@@tinymce',
-                {'fieldname': answerid},
-                function() {
-                    var config = new TinyMCEConfig(answerid);
-                    config.init();
+        answercount += 1;
+        answerid = 'answer' + answercount;
 
+        $.ajax({
+            url: '@@upfront.assessmentitem.answerform',
+            data: {'answerid': answerid},
+            success: function(data) {
+                $("#answer_listing").append(data);
+                tinymceid = 'form.widgets.' + answerid + '.answer';
+                var config = new TinyMCEConfig(tinymceid);
+                config.init();
+                $('#' + answerid + " .delete").click(function() {
+                    $(this).parent().remove();    
                 });
-            jq("#result_choice").load('++add++upfront.assessmentitem.content.answer #formfield-form-widgets-iscorrect');
-
-            // place formatted ajax result in appropriate place
-            jq("#answer_listing ul").append(jq('#answer_template li'));
-
-            //remove result_answer and result_choice id from placed ajax results. (to not confuse next ajax request)
-            jq("#answer_listing #result_answer").removeAttr('id');
-            jq("#answer_listing #result_choice").removeAttr('id');
-
-            // show the delete answer button
-            jq("#delete_last_answer").removeClass('hidden');
-            
-        }
-    });
-
-    jq("#delete_last_answer").click(function() {
-
-        jq("#answer_listing li:last").detach();
-
-        //hide delete answer button if no answers exist
-        if (jq("#answer_listing li").length == 0) {
-            jq("#delete_last_answer").addClass('hidden');
-        }
+            }
+        });
 
     });
 
