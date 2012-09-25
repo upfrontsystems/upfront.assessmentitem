4cd50f67aa0e496f6527484dd43e271acc1faa50
Roché Compaan <roche@upfrontsystems.co.za>
2012-09-25 10:49:10 +0200

    initialize tinymce for each answer
    update answercount on load

--- upfront/assessmentitem/static/js/assessmentitem.js
+++ upfront/assessmentitem/static/js/assessmentitem.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     var url = location.href;
 
+    var answercount = 0;
+
     $('.assessmentitem-edit-link').prepOverlay({
         subtype: 'ajax',
         filter: '#content>*',
@@ -12,16 +14,23 @@ $(document).ready(function() {
         config: {
             onLoad: function() {
                 $("#add_answer").click(add_answer);
-                tinymceid = 'form.widgets.question';
+                var tinymceid = 'form.widgets.question';
                 delete InitializedTinyMCEInstances[tinymceid];
                 var config = new TinyMCEConfig(tinymceid);
                 config.init();
+                answercount = 0;
+
+                $("#answer_listing textarea.mce_editable").each(
+                    function(index) {
+                        answercount += answercount;
+                        config = new TinyMCEConfig($(this).attr('id'));
+                        config.init();
+                    }
+                );
             }
         }
     });
 
-
-    var answercount = 0;
 
     function add_answer() {
         answercount += 1;
@@ -32,7 +41,7 @@ $(document).ready(function() {
             data: {'answerid': answerid},
             success: function(data) {
                 $("#answer_listing").append(data);
-                tinymceid = 'form.widgets.' + answerid + '-answer';
+                var tinymceid = 'form.widgets.' + answerid + '-answer';
                 var config = new TinyMCEConfig(tinymceid);
                 config.init();
                 $('#' + answerid + " .delete").click(function() {
