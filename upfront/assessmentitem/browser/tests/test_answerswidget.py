30a94d1413bc9a0c12fa4adcedf419c2d7985ef7
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-29 13:43:44 +0200

    browser tests

---
+++ upfront/assessmentitem/browser/tests/test_answerswidget.py
@@ -0,0 +1,7 @@
+from upfront.assessmentitem.tests.base import UpfrontAssessmentItemTestBase
+
+class TestAnswerWidget(UpfrontAssessmentItemTestBase):
+    """ Test answer widget """
+    
+    def test_widget(self):
+        pass
